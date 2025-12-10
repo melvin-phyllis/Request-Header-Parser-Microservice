ff012a7dd03521d796db9d9e7ecbeedfe760700c
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-12-10 23:44:18 +0000

    Request Header Parser Microservice V1

--- routes/routes.js
+++ routes/routes.js
@@ -21,13 +21,21 @@ routes.get('/api/hello', function (req, res) {
 
 routes.get("/whoami", (req, res) => {
     /*  {
-         "ipaddress": "159.20.14.100", "language": "en-US,en;q=0.5",
-             "software": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
-     } */
-    res.json({
-        "ipaddress": req.ip, "language": req.headers["accept-language"],
-        "software": req.headers["user-agent"]
-    })
-   
+        "ipaddress":req.ip,
+
+        "language":req.headers["accept-language"],
+
+        "software":req.headers["user-agent"]
+    } */
+    res.json(
+        {
+            "ipaddress": req.ip,
+
+            "language": req.headers["accept-language"],
+
+            "software": req.headers["user-agent"]
+        }
+    )
+
 })
 export default routes
